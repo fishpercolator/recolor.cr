e0725edce1d7639c76148ae35c2e41c7855bc230
Rich Daley <rich@fishpercolator.co.uk>
2017-07-14 15:47:20 +0100

    Introduce turbolinks

--- public/js/recolor.js
+++ public/js/recolor.js
@@ -1,24 +1,3 @@
-Dropzone.options.drop = {
-  paramName: 'svg',
-  acceptedFiles: 'image/svg+xml',
-  dictDefaultMessage: 'Now drag black SVGs here',
-  thumbnailWidth: 64,
-  thumbnailHeight: 64,
-  maxFilesize: 1, // up to 1MB allowed 
-  init: function() {
-    this.on('success', function (file, response) {
-      var output = $('<div class="output"></div>');
-      var img = $('<img/>').attr({src: response.data});
-      var link = $('<a></a>').attr({download: response.filename, href: response.data}).text(response.filename);
-      output.append(img).append(link);
-      $('#response').append(output);
-      setTimeout(function () {
-        $(file.previewElement).fadeOut();
-      }, 1000);
-    });
-  }
-}
-
 // From clrs.cc
 var PALETTE = [  
   ['#dddddd', '#aaaaaa'],
@@ -32,8 +11,7 @@ var PALETTE = [
 ];
 var PALETTE_FLATTENED = [].concat.apply([], PALETTE);
 
-$(document).ready(function () {
-  
+function init_spectrum() {
   var random_color = PALETTE_FLATTENED[Math.floor(Math.random() * PALETTE_FLATTENED.length)];
   $('#color').spectrum({
     color: random_color,
@@ -48,5 +26,33 @@ $(document).ready(function () {
     }
   });
   $('input[name="color"]').val(random_color);
-  
+}
+
+function init_dropzone() {
+  Dropzone.options.drop = {
+    paramName: 'svg',
+    acceptedFiles: 'image/svg+xml',
+    dictDefaultMessage: 'Now drag black SVGs here',
+    thumbnailWidth: 64,
+    thumbnailHeight: 64,
+    maxFilesize: 1, // up to 1MB allowed 
+    init: function() {
+      this.on('success', function (file, response) {
+        var output = $('<div class="output"></div>');
+        var img = $('<img/>').attr({src: response.data});
+        var link = $('<a></a>').attr({download: response.filename, href: response.data}).text(response.filename);
+        output.append(img).append(link);
+        $('#response').append(output);
+        setTimeout(function () {
+          $(file.previewElement).fadeOut();
+        }, 1000);
+      });
+    }
+  }
+  $('#drop').dropzone();
+}
+
+$(document).on('turbolinks:load', function () {
+  init_spectrum();
+  init_dropzone();
 });
