6acd41bc7df6f72d92d3fc4c588b5565ce84e480
Rich Daley <rich@fishpercolator.co.uk>
2017-09-20 14:14:52 +0100

    Fix a bug and rely on heroku to run webpack for us

--- js/recolor.js
+++ js/recolor.js
@@ -17,44 +17,48 @@ var PALETTE = [
 var PALETTE_FLATTENED = [].concat.apply([], PALETTE);
 
 function init_spectrum() {
-  var random_color = PALETTE_FLATTENED[Math.floor(Math.random() * PALETTE_FLATTENED.length)];
-  $('#color').spectrum({
-    color: random_color,
-    showPalette: true,
-    showInput: true,
-    preferredFormat: 'hex',
-    palette: PALETTE,
-    clickoutFiresChange: true,
-    showButtons: true,
-    maxSelectionSize: 3,
-    change: function (color) {
-      $('input[name="color"]').val(color);
-    }
-  });
-  $('input[name="color"]').val(random_color);
+  if ($('#color').length && !$('.sp-replacer').length) {
+    var random_color = PALETTE_FLATTENED[Math.floor(Math.random() * PALETTE_FLATTENED.length)];
+    $('#color').spectrum({
+      color: random_color,
+      showPalette: true,
+      showInput: true,
+      preferredFormat: 'hex',
+      palette: PALETTE,
+      clickoutFiresChange: true,
+      showButtons: true,
+      maxSelectionSize: 3,
+      change: function (color) {
+        $('input[name="color"]').val(color);
+      }
+    });
+    $('input[name="color"]').val(random_color);
+  }
 }
 
 function init_dropzone() {
-  new Dropzone("form#drop", {
-    paramName: 'svg',
-    acceptedFiles: 'image/svg+xml',
-    dictDefaultMessage: 'Now drag black SVGs here',
-    thumbnailWidth: 64,
-    thumbnailHeight: 64,
-    maxFilesize: 1, // up to 1MB allowed 
-    init: function() {
-      this.on('success', function (file, response) {
-        var output = $('<div class="output"></div>');
-        var img = $('<img/>').attr({src: response.data});
-        var link = $('<a></a>').attr({download: response.filename, href: response.data}).text(response.filename);
-        output.append(img).append(link);
-        $('#response').append(output);
-        setTimeout(function () {
-          $(file.previewElement).fadeOut();
-        }, 1000);
-      });
-    }
-  });
+  if ($('form#drop').length && !$('form#drop').hasClass('dz-clickable')) {
+    new Dropzone("form#drop", {
+      paramName: 'svg',
+      acceptedFiles: 'image/svg+xml',
+      dictDefaultMessage: 'Now drag black SVGs here',
+      thumbnailWidth: 64,
+      thumbnailHeight: 64,
+      maxFilesize: 1, // up to 1MB allowed 
+      init: function() {
+        this.on('success', function (file, response) {
+          var output = $('<div class="output"></div>');
+          var img = $('<img/>').attr({src: response.data});
+          var link = $('<a></a>').attr({download: response.filename, href: response.data}).text(response.filename);
+          output.append(img).append(link);
+          $('#response').append(output);
+          setTimeout(function () {
+            $(file.previewElement).fadeOut();
+          }, 1000);
+        });
+      }
+    });
+  }
 }
 
 $(document).on('turbolinks:load', function () {
